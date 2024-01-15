1178d2725f8516a0ff18b819229abb9d98d21518
FadedGaming <59406489+FadedGaming@users.noreply.github.com>
2024-01-15 21:33:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = [".gg/darktoolz"];
 
 
 let currentIndex = 0;
